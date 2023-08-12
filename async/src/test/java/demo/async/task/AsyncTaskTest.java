d31301a0740aa833a300a14640b9f162f8edc157
relinamarshall <relinamarshall@gmail.com>
2023-08-12 17:40:13 +0800

    Async CompletableFuture Test Demo

--- async/src/test/java/demo/async/task/AsyncTaskTest.java
+++ async/src/test/java/demo/async/task/AsyncTaskTest.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.Future;
 
 import demo.async.AsyncApplicationTest;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -29,16 +30,17 @@ public class AsyncTaskTest extends AsyncApplicationTest {
      * 测试异步任务
      */
     @Test
-    public void asyncTaskTest() throws InterruptedException {
+    @SneakyThrows
+    public void asyncTaskTest() {
         long start = System.currentTimeMillis();
         Future<Boolean> asyncTask1 = task.asyncTask1();
         Future<Boolean> asyncTask2 = task.asyncTask2();
         Future<Boolean> asyncTask3 = task.asyncTask3();
 
         // 调用 get() 阻塞主线程
-        //asyncTask1.get();
-        //asyncTask2.get();
-        //asyncTask3.get();
+        asyncTask1.get();
+        asyncTask2.get();
+        asyncTask3.get();
         long end = System.currentTimeMillis();
 
         log.info("异步任务全部执行结束，总耗时：{} 毫秒", (end - start));
